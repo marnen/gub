956e455f1cb4abe8c2c38d7d26adf28b6f8e70bc
Jahrme Risner <github@jahrme.com>
2019-02-07 13:02:55 -0800

    Fix osx-lilypad Specification

--- gub/specs/osx-lilypad.py
+++ gub/specs/osx-lilypad.py
@@ -1,12 +1,10 @@
 from gub import build
 
 class Osx_lilypad (build.NullBuild):
-    pass
+    source = 'http://lilypond.org/downloads/gub-sources/osx-lilypad-universal/osx-lilypad-universal-0.6.3.tar.gz'
 
 class Osx_lilypad__darwin__ppc (build.NullBuild):
     # yes, we really need the old version here
     # later versions were built on OSX 10.7, which doesn't have pcc support
     source = 'http://lilypond.org/downloads/gub-sources/osx-lilypad-universal/osx-lilypad-universal-0.4.tar.gz'
 
-class Osx_lilypad__darwin__x86 (build.NullBuild):
-    source = 'http://lilypond.org/downloads/gub-sources/osx-lilypad-universal/osx-lilypad-universal-0.6.3.tar.gz'
