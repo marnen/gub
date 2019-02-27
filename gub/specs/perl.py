f6a3a9628d21aedeae6e5daa4c8cd04627506b46
Jahrme Risner <github@jahrme.com>
2019-02-27 14:45:02 -0800

    Clean up perl spec; closer to compiling on mac.

--- gub/specs/perl.py
+++ gub/specs/perl.py
@@ -1,6 +1,6 @@
 import re
-#
-from gub import misc
+
+from gub import build_platform
 from gub import tools
 
 # And this is supposed to be one of the most compiled packages?
@@ -8,32 +8,38 @@ from gub import tools
 class Perl__tools (tools.AutoBuild):
     source = 'http://www.cpan.org/src/5.0/perl-5.10.0.tar.gz'
     srcdir_build_broken = True
+    configure_binary = '%(autodir)s/configure.gnu'
+
+    if 'darwin' in build_platform.machine():
+        librt_flag = '-lSystem'
+    else:
+        librt_flag = '-lrt'
+
+    configure_command = ' '.join([
+        '%(configure_binary)s',
+        '-Aldflags="%(rpath)s"',
+        '-Alibs="-lm -ldl %(librt_flag)s"',
+        '-Dcc="%(toolchain_prefix)sgcc %(target_gcc_flags)s"',
+        '-Dcccdlflags=-fPIC',
+        '-Dincpth=/',
+        '-Dlibperl=libperl.so',
+        '-Dlibpth=%(system_prefix)s/lib',
+        '-Dlocallibpth=/',
+        '-Dprefix=%(system_prefix)s',
+        '-Dsitearch=%(system_prefix)s/lib/perl5/5.10.0',
+        '-Dsitelib=%(system_prefix)s/lib/perl5/5.10.0',
+        '-Dtargetarch=%(target_architecture)s',
+        '-Dusedl',
+        '-Duseshrplib',
+        '-Dusrinc=%(system_prefix)s/include',
+    ])
+
     def patch (self):
         tools.AutoBuild.patch (self)
         self.file_sub ([('-c (/dev/null)', r'-e \1')], '%(srcdir)s/Configure')
-    configure_binary = '%(autodir)s/configure.gnu'
-# -Dcc=%(CC)s
-# -Dprefix=%(prefix_dir)s -- BOOTSTRAP
-    configure_command = misc.join_lines ('''%(configure_binary)s
- -Dprefix=%(system_prefix)s
- -Dcc='%(toolchain_prefix)sgcc %(target_gcc_flags)s'
- -Dtargetarch=%(target_architecture)s
- -Dusrinc=%(system_prefix)s/include
- -Dincpth=/
- -Dlibpth=%(system_prefix)s/lib
- -Dsitelib=%(system_prefix)s/lib/perl5/5.10.0
- -Dsitearch=%(system_prefix)s/lib/perl5/5.10.0
- -Dusedl
- -Duseshrplib
- -Dlibperl=libperl.so
- -Dcccdlflags=-fPIC
- -Dlocallibpth=/
- -Aldflags='%(rpath)s'
- -Alibs='-lm -lrt -ldl'
-''')
+
     def configure (self):
         tools.AutoBuild.configure (self)
         for i in ['%(builddir)s/makefile', '%(builddir)s/x2p/makefile']:
             # Ugh, missing some command?
             self.file_sub ([('^0$','')], i)
-#    def install_command (self):
