8a37685a6df8a457aea64bae97599d9832aaec63
Marnen Laibow-Koser <marnen@marnen.org>
2019-10-16 16:16:50 +0100

    Use HTTPS download since HTTP no longer works.

--- gub/specs/xzutils.py
+++ gub/specs/xzutils.py
@@ -2,5 +2,5 @@
 from gub import tools
 
 class Xzutils__tools (tools.AutoBuild):
-    source = 'http://tukaani.org/xz/xz-5.0.7.tar.bz2'
+    source = 'https://tukaani.org/xz/xz-5.0.7.tar.bz2'
     dependencies = ['tools::bzip2']
