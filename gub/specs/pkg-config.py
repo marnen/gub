7ff441fc90661d9881596a0697ea2830671e1258
Marnen Laibow-Koser <marnen@marnen.org>
2019-10-18 19:28:47 +0100

    Massage options for pkg-config on Darwin.

--- gub/specs/pkg-config.py
+++ gub/specs/pkg-config.py
@@ -1,3 +1,4 @@
+from gub import build_platform
 from gub import tools
 
 class Pkg_config__tools (tools.AutoBuild):
@@ -5,3 +6,8 @@ class Pkg_config__tools (tools.AutoBuild):
             'libtool',
             'system::g++',
             ]
+
+    def __init__ (self, settings, source):
+        tools.AutoBuild.__init__ (self, settings, source)
+        if 'darwin' in build_platform.machine():
+            self.configure_variables += 'CFLAGS="-I%(system_prefix)s/include -std=gnu89"'
